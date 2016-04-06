9bea2027ef8f0eab4100a82e6d3632c5346b36aa
Alexander Wheadon <alexanderwheadon@gmail.com>
2016-04-06 12:17:39 -0400

    Update daily.sh

--- Phase6_unified/daily.sh
+++ Phase6_unified/daily.sh
@@ -3,10 +3,12 @@ front="../../Phase6_unified/frontend/Phase6_frontend_unix"
 backdir="../../Phase6_unified/"
 backrun="Phase4.Main"
 day="day$1"
+nextday="day$2"
 masterAccount="../../Phase6_unified/$day/master_accounts.txt"
 masterTransaction="../../Phase6_unified/$day/transConcat.txt"
 
 cd $day
+
 #Loop through input files for front end
 for f in *.in;
 do
@@ -19,5 +21,10 @@ done
 cat trans*.txt > transConcat.txt
 #back end
 java -cp $backdir $backrun $masterAccount $masterTransaction
-
-#rm transConcat.txt
+#transfer between days
+if [ "$nextday" != "day0" ]; then	
+	cp transConcat.txt ../$nextday/
+	cp current_accounts.txt ../$nextday/
+	cp master_accounts.txt ../$nextday/
+fi
+cd ..
